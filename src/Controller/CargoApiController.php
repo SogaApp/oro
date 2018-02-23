f7d4d0b42ce11a0b081c43ec788f191b2f4d6d34
juan <felipemesa14@gmail.com>
2018-02-23 13:54:31 -0500

    Commit inicial del proyecto en symfony 4

--- src/Controller/CargoApiController.php
+++ src/Controller/CargoApiController.php
@@ -2,26 +2,31 @@
 
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use FOS\RestBundle\Controller\FOSRestController;
 use FOS\RestBundle\View\View;
+use Symfony\Component\HttpFoundation\Response;
 
-class CargoApiController extends FOSRestController {
+class CargoApiController extends FOSRestController
+{
 
-	/**
-	 * @Rest\Get("/api/lista/cargo")
-	 */
-	public function lista( Request $request) {
+    /**
+     * @Rest\Get("/api/lista/cargo")
+     */
+    public function lista(Request $request)
+    {
 
 
-		$restresult = $this->getDoctrine()->getRepository('App:Cargo')->findAll();
+        $restresult = $this->getDoctrine()->getRepository('App:Cargo')->findAll();
 
-		if ($restresult === null) {
-			return new View("No hay cargos", Response::HTTP_NOT_FOUND);
-		}
-		return $restresult;
-	}
+        if ($restresult === null) {
+            return new View("No hay cargos", Response::HTTP_NOT_FOUND);
+        }
+
+        return $restresult;
+    }
 
 
 }
