ae85ad8b41acac6674ebc716914f250951d902b6
Juan David Marulanda Velasquez <jadamavecool@gmail.com>
2018-02-26 16:56:50 -0500

    Test DataTable on casos

--- public/local/assets/js/oro.js
+++ public/local/assets/js/oro.js
@@ -1,5 +1,22 @@
-function abrirVentana(url, Nombre, Alto, Ancho) {
-    windowObjectReference2 = window.open(url, Nombre, "toolbar=0, width=" + Ancho + ", height=" + Alto + ",location=0,status=1,menubar=0,scrollbars=1,resizable=0");
-    windowObjectReference2.focus();
-}
+jQuery(document).ready(function($){
+    function abrirVentana(url, Nombre, Alto, Ancho) {
+        windowObjectReference2 = window.open(url, Nombre, "toolbar=0, width=" + Ancho + ", height=" + Alto + ",location=0,status=1,menubar=0,scrollbars=1,resizable=0");
+        windowObjectReference2.focus();
+    }
+
+    $("#casos-general").DataTable({
+        "language": {
+            "lengthMenu": "Mostrando _MENU_ registros por página",
+            "zeroRecords": "No hay Registros :(",
+            "info": "Mostrando página _PAGE_ de _PAGES_",
+            "infoEmpty": "Información no disponible o no existen registros con esas características",
+            "infoFiltered": "(Filtrados de _MAX_ total de registros)",
+            "prev" : "Anterior",
+            "next" : "Siguiente",
+            "search": "Buscar"
+        }
+    });
+});
+
+
 
